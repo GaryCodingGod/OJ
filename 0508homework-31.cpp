ec503274162ee995ef92e3710a39e9386ace59fa
GaryCodingGod <y.r82009@gmail.com>
2024-05-09 18:42:53 +0800

    OJ homework

---
+++ 0508homework-31.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+template <typename T>
+void sort(T *array, int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (array[j] > array[j + 1])
+            {
+                T temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+            }
+        }
+    }
+}
